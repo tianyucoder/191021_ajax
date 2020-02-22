8b66eb8c2923128a9cb23d1623cead7d63a6b0d0
tianyu5177 <zhangtianyu5177@sina.com>
2020-02-22 17:15:38 +0800

    'init'

---
+++ 1.原生js发送ajax请求/server.js
@@ -0,0 +1,21 @@
+//引入express
+const express = require('express')
+//实例化一个app应用对象
+const app = express()
+//使用中间件暴露静态资源
+app.use(express.static(__dirname+'/public'))
+
+
+//配置路由
+app.get('/demo',(request,response)=>{
+  console.log('有人发来了get请求，我收到了')
+  response.send('ok')
+})
+
+//绑定端口监听
+app.listen(3000,(err)=>{
+  if(!err){
+    console.log('兄弟不要用编译器打开页面')
+    console.log('练习原生js发送get请求的地址是：http://localhost:3000/ajax_get.html')
+  }
+})
